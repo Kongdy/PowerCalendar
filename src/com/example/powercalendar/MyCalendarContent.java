7e6384ea5ee65febc790d9b5c597ed8b6d0be10f
kongdy <906011748@qq.com>
2016-03-30 17:31:16 +0800

    啊啊啊啊啊啊啊啊啊啊啊啊啊

--- src/com/example/powercalendar/MyCalendarContent.java
+++ src/com/example/powercalendar/MyCalendarContent.java
@@ -12,87 +12,82 @@ import com.example.powercalendar.tools.Utils;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Rect;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * 日历的主题可滑动部分
+ * 
  * @author wangk
  *
  */
-public class MyCalendarContent extends ScrollView {
-	
+public class MyCalendarContent extends ViewGroup {
+
 	private final static int MATHCH_PARENT = ViewGroup.LayoutParams.MATCH_PARENT;
 	private final static int WARP_CONTENT = ViewGroup.LayoutParams.WRAP_CONTENT;
-	
+
 	private MyCalendarLayout mCalendarM1; // 每页，复用控件1
 	private MyCalendarLayout mCalendarM2; // 每页，复用控件2
 	private MyCalendarLayout mCalendarM3; // 每页，复用控件3
 	private MyCalendarLayout mCalendarMCursor; // 每页，复用控件4
-	
+
 	private CopyOnWriteArrayList<MyCalendarLayout> mCalendars; // 控件组
-	
-	private final int mCalendarM1_ID = 1;
-	private final int mCalendarM2_ID = 2;
-	private final int mCalendarM3_ID = 3;
-	private final int mCalendarMCursor_ID = 4;
-	
+
+	private int MAX_ID;
+
 	private LinearLayout mainContent; // 主布局
 
-	private  int MAX_TAB_WIDTH;
+	private int MAX_TAB_WIDTH;
 
 	private Context context;
+
+	private int currentChangeMonth; // 当前改变月份
+
+	private int mSCROLL_STATU; // 滚动状态
+
+	private int minSubStractMonth;// 最小月份相减
 	
-	private int currentMaxMonth; // 当前最大月份
-	
-	private int mChildCount; // 复用控件数量
-	
+	private float TouchX;
+	private float TouchY;
+
 	public MyCalendarContent(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.context = context;
 		notifyDataSetChanged();
 	}
-	
-	
+
 	public MyCalendarContent(Context context) {
 		super(context);
 		this.context = context;
 		notifyDataSetChanged();
 	}
 
-
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 	}
-	
+
 	/**
 	 * 刷新/注入数据,生成万年历
 	 */
 	public void notifyDataSetChanged() {
-		
-		
-		// TODO 硬编码
-		mChildCount = 4;
-		
 		mCalendars = new CopyOnWriteArrayList<MyCalendarLayout>();
 		mainContent = new LinearLayout(context);
 		mainContent.setOrientation(LinearLayout.VERTICAL);
 		setVerticalScrollBarEnabled(false);// 不显示滚动条(垂直方向)
 		setBackgroundColor(Color.WHITE);
 		createRecycleMonth();
-		
 	}
-	
+
 	/**
 	 * 创建三个月份，上中下三层，每次在滑动的过程中复用这三个布局
 	 */
@@ -101,58 +96,83 @@ public class MyCalendarContent extends ScrollView {
 		mCalendarM2 = new MyCalendarLayout(context);
 		mCalendarM3 = new MyCalendarLayout(context);
 		mCalendarMCursor = new MyCalendarLayout(context);
-		
-		mCalendarM1.setId(mCalendarM1_ID);
-		mCalendarM2.setId(mCalendarM2_ID);
-		mCalendarM3.setId(mCalendarM3_ID);
-		mCalendarMCursor.setId(mCalendarMCursor_ID);
-		
+
 		mCalendars.add(mCalendarM1);
 		mCalendars.add(mCalendarM2);
 		mCalendars.add(mCalendarM3);
 		mCalendars.add(mCalendarMCursor);
-		
-		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(MATHCH_PARENT, WARP_CONTENT);
+
+		minSubStractMonth = 0;
+		currentChangeMonth = 0;
+		MAX_ID = 1;
+
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+				MATHCH_PARENT, WARP_CONTENT);
 		int i = 0;
-		while(i < mCalendars.size()) {
-			createMonth(mCalendars.get(i),System.currentTimeMillis(),i-1);
-			mainContent.addView(mCalendars.get(i),params);
+		while (i < mCalendars.size()) {
+			createMonth(mCalendars.get(i), System.currentTimeMillis(), i - 1);
+			mainContent.addView(mCalendars.get(i), params);
 			i++;
 		}
 		addView(mainContent);
 	}
-	
+
 	/**
 	 * 给月份填充数据
+	 * 
 	 * @param mCalendarM
 	 * @param list
 	 * @param timeMills
 	 */
-	private void fillData(MyCalendarLayout mCalendarM,List<Map<String, String>> list,long TimeMills,int changeMonth) {
+	private void fillData(MyCalendarLayout mCalendarM,
+			List<Map<String, String>> list, long TimeMills, int changeMonth) {
 		MyCalendarWeek mCalendarW = new MyCalendarWeek(context); // 每行
 		Calendar c = Calendar.getInstance();
 		c.setTimeInMillis(TimeMills);
-		int month = c.get(Calendar.MONTH)+1;
-		currentMaxMonth= month;
+		int month = c.get(Calendar.MONTH) + 1;
+
+		if (minSubStractMonth > changeMonth) {
+			minSubStractMonth = changeMonth;
+		}
+		if (currentChangeMonth < changeMonth) {
+			currentChangeMonth = changeMonth;
+		}
 		for (int i = 0; i < list.size(); i++) {
 			Map<String, String> map = list.get(i);
 			int weekday = Integer.parseInt(map.get("week"));
 			// 填充空白,添加月份标题
-			if( i == 0 ) {
-				int count = weekday-1;
+			if (i == 0) {
+				int count = weekday - 1;
+				if (month == 1) {
+					int year = c.get(Calendar.YEAR);
+					TextView mCalendarYear = new TextView(context);
+					mCalendarYear.setText("  " + year + "年");
+					mCalendarYear.setTextColor(getResources().getColor(
+							R.color.red));
+					// 加粗
+					TextPaint tp = mCalendarYear.getPaint();
+					tp.setFakeBoldText(true);
+					mCalendarYear.setTextSize(Utils.getRawSize(getContext(),
+							TypedValue.COMPLEX_UNIT_SP, 8));
+					mCalendarW.addView(mCalendarYear);
+					mCalendarM.addView(mCalendarW);
+					mCalendarW = new MyCalendarWeek(context);
+				}
 				for (int j = 0; j < count; j++) {
 					MyCalendarDay mCalendarD = new MyCalendarDay(context);
 					mCalendarD.setText("  ");
 					mCalendarW.addView(mCalendarD);
 				}
 				MyCalendarDay mCalendarTitle = new MyCalendarDay(context);
-				mCalendarTitle.setText(month+"月");
-				mCalendarTitle.setTextColor(getResources().getColor(R.color.text_orange));
-				mCalendarTitle.setTextSize(Utils.getRawSize(context, TypedValue.COMPLEX_UNIT_SP, 6));
+				mCalendarTitle.setText(month + "月");
+				mCalendarTitle.setTextColor(getResources().getColor(
+						R.color.text_orange));
+				mCalendarTitle.setTextSize(Utils.getRawSize(context,
+						TypedValue.COMPLEX_UNIT_SP, 6));
 				mCalendarW.addView(mCalendarTitle);
 				mCalendarM.addView(mCalendarW);
-				mCalendarW = new MyCalendarWeek(context); 
-				if(weekday!=1) {
+				mCalendarW = new MyCalendarWeek(context);
+				if (weekday != 1) {
 					for (int j = 0; j < count; j++) {
 						MyCalendarDay mCalendarD = new MyCalendarDay(context);
 						mCalendarD.setText("  ");
@@ -161,88 +181,156 @@ public class MyCalendarContent extends ScrollView {
 				}
 			}
 			MyCalendarDay mCalendarD = new MyCalendarDay(context); // 每天
-			if(weekday == 1 || weekday == 7) {
-				mCalendarD.setTextColor(getResources().getColor(R.color.title_weekend));
+			if (weekday == 1 || weekday == 7) {
+				mCalendarD.setTextColor(getResources().getColor(
+						R.color.title_weekend));
 			}
 			mCalendarD.setText(map.get("day"));
-			mCalendarD.setTextSize(Utils.getRawSize(context, TypedValue.COMPLEX_UNIT_SP,7 ));
+			mCalendarD.setTextSize(Utils.getRawSize(context,
+					TypedValue.COMPLEX_UNIT_SP, 7));
 			mCalendarW.addView(mCalendarD);
-			if( weekday%7 == 0 || i >= list.size()-1 ) {
+			if (weekday % 7 == 0 || i >= list.size() - 1) {
 				mCalendarM.addView(mCalendarW);
 				mCalendarW = new MyCalendarWeek(context);
 			}
 		}
 	}
-	
+
 	/**
-	 *  通过时间戳获取某个月的带有星期的天数
-	 * @param TimeMills 时间毫秒
-	 * @param changeMonth 月份变化，如 -1为这个时间的上个月，1为这个时间的下个月
+	 * 通过时间戳获取某个月的带有星期的天数
+	 * 
+	 * @param TimeMills
+	 *            时间毫秒
+	 * @param changeMonth
+	 *            月份变化，如 -1为这个时间的上个月，1为这个时间的下个月
 	 * @return
 	 */
-	private List<Map<String, String>> createMonth(MyCalendarLayout mCalendarM,long TimeMills,int changeMonth) {
-		List<Map<String, String>> list = new ArrayList<Map<String,String>>();
+	private List<Map<String, String>> createMonth(MyCalendarLayout mCalendarM,
+			long TimeMills, int changeMonth) {
+		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
 		Calendar c = Calendar.getInstance();
-		if(TimeMills >= 0l) {
+		if (TimeMills >= 0l) {
 			c.setTimeInMillis(TimeMills);
 		}
-		if(changeMonth != 0) {
+		if (changeMonth != 0) {
 			c.add(Calendar.MONTH, changeMonth);
 		}
+		mCalendarM.setId(MAX_ID + 1);
 		int maxDay = c.getActualMaximum(Calendar.DAY_OF_MONTH);
 		int firstWeekDay = getFirstWeekDayOfMonth(c.getTimeInMillis());
 		for (int i = 0; i < maxDay; i++) {
 			Map<String, String> map = new HashMap<String, String>();
-			int day = i+1;
-			map.put("day", ""+day);
-			map.put("week", ""+firstWeekDay);
+			int day = i + 1;
+			map.put("day", "" + day);
+			map.put("week", "" + firstWeekDay);
 			list.add(map);
 			++firstWeekDay;
-			if(firstWeekDay > 7) {
+			if (firstWeekDay > 7) {
 				firstWeekDay = 1;
 			}
 		}
-		if(list != null && list.size() > 0) {
+		if (list != null && list.size() > 0) {
 			fillData(mCalendarM, list, c.getTimeInMillis(), changeMonth);
 		}
 		return list;
 	}
-	
+
 	private int getFirstWeekDayOfMonth(long TimeMills) {
 		Calendar c = Calendar.getInstance();
 		c.setTimeInMillis(TimeMills);
 		c.set(Calendar.DATE, 1);
 		return c.get(Calendar.DAY_OF_WEEK);
 	}
-	
-	
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			TouchX = event.getX();
+			TouchY = event.getY();
+			break;
+		case MotionEvent.ACTION_UP:
+
+			break;
+		case MotionEvent.ACTION_MOVE:
+
+			break;
+		default:
+			break;
+		}
+
+		return super.onTouchEvent(event);
+	}
+
 	@Override
 	protected void onScrollChanged(int l, int t, int oldl, int oldt) {
 		super.onScrollChanged(l, t, oldl, oldt);
+		if (t > oldt) {
+			mSCROLL_STATU = SCROLL_STATE.STATE_SCROLLED_UP.nativeInt;
+		} else {
+			mSCROLL_STATU = SCROLL_STATE.STATE_SCROLLED_DONW.nativeInt;
+		}
+
+	}
+
+	// TODO
+	private void refreshView() {
+
 		int[] location = new int[2];
-		int i = 0;
-		while(i < mCalendars.size()) {
+		int[] location2 = new int[2];
+		int i = mCalendars.size() - 1;
+		MyCalendarLayout endM = (MyCalendarLayout) mainContent.getChildAt(i);
+		endM.getLocationOnScreen(location);
+		MyCalendarLayout firstM = (MyCalendarLayout) mainContent.getChildAt(0);
+		firstM.getLocationOnScreen(location2);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+				MATHCH_PARENT, WARP_CONTENT);
+		if (mSCROLL_STATU == SCROLL_STATE.STATE_SCROLLED_UP.nativeInt) {
 			MyCalendarLayout m = mCalendars.get(i);
 			m.getLocationOnScreen(location);
-			if((location[1]+m.getHeight())<0) {
-				mCalendars.remove(i);
-				createMonth(m, System.currentTimeMillis(), currentMaxMonth+1);
-				mCalendars.add(m);
+			if ((location2[1] + firstM.getHeight()) < 0
+					&& location[1] < getHeight()) {
+				int id = mCalendars.get(mCalendars.size() - 1).getId();
+				MyCalendarLayout newM = new MyCalendarLayout(getContext());
+				params.addRule(RelativeLayout.BELOW, id);
+				createMonth(newM, System.currentTimeMillis(),
+						currentChangeMonth + 1);
+				mainContent.addView(newM, params);
+				mCalendars.add(newM);
+				System.out.println("add foot");
 			}
-			i++;
+		} else if (mSCROLL_STATU == SCROLL_STATE.STATE_SCROLLED_DONW.nativeInt) {
+			if (location[1] > getHeight()
+					&& location2[1] + firstM.getHeight() > 0) {
+				// System.out.println("getHeight():"+getHeight()+"location[1]:"+location[1]+",i==="+i);
+				int id = mCalendars.get(0).getId();
+				params.addRule(RelativeLayout.ABOVE, id);
+				MyCalendarLayout newM = new MyCalendarLayout(getContext());
+				createMonth(newM, System.currentTimeMillis(),
+						minSubStractMonth - 1);
+				mainContent.addView(newM, 0, params);
+				mCalendars.add(0, newM);
+				System.out.println("add head");
+			}
 		}
+		mSCROLL_STATU = SCROLL_STATE.STATE_STATIC.nativeInt;
 	}
-	
+
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-		boolean fillViewFlag = widthMode == MeasureSpec.EXACTLY;
-		setFillViewport(fillViewFlag);
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+		measureChildren(widthMeasureSpec, heightMeasureSpec);
+
+		setMeasuredDimension(widthSize, heightSize);
+		// super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 	}
 
 	/**
 	 * 每行日期
+	 * 
 	 * @author wangk
 	 *
 	 */
@@ -257,23 +345,26 @@ public class MyCalendarContent extends ScrollView {
 			super(context);
 			init();
 		}
-		
+
 		private void init() {
 			setOrientation(LinearLayout.HORIZONTAL);
-			setPadding(0, (int)Utils.getRawSize(context, TypedValue.COMPLEX_UNIT_DIP, 15), 0, 
-					(int)Utils.getRawSize(context, (int)TypedValue.COMPLEX_UNIT_DIP, 15));
+			setPadding(0, (int) Utils.getRawSize(context,
+					TypedValue.COMPLEX_UNIT_DIP, 15), 0,
+					(int) Utils.getRawSize(context,
+							(int) TypedValue.COMPLEX_UNIT_DIP, 15));
 		}
-		
+
 		@Override
 		protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-			MAX_TAB_WIDTH = MeasureSpec.getSize(widthMeasureSpec)/7;
+			MAX_TAB_WIDTH = MeasureSpec.getSize(widthMeasureSpec) / 7;
 			super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 		}
-		
+
 	}
-	
+
 	/**
 	 * 每天日期
+	 * 
 	 * @author wangk
 	 */
 	public class MyCalendarDay extends TextView {
@@ -287,12 +378,66 @@ public class MyCalendarContent extends ScrollView {
 			super(context);
 			setGravity(Gravity.CENTER);
 		}
-		
+
 		@Override
 		protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-			super.onMeasure(MeasureSpec.makeMeasureSpec(MAX_TAB_WIDTH, MeasureSpec.EXACTLY),
-					heightMeasureSpec);
+
+			super.onMeasure(MeasureSpec.makeMeasureSpec(MAX_TAB_WIDTH,
+					MeasureSpec.EXACTLY), heightMeasureSpec);
 		}
-		
+
+	}
+
+	/**
+	 * scrollview滚动状态枚举
+	 * 
+	 * @author wangk
+	 *
+	 */
+	static enum SCROLL_STATE {
+		/**
+		 * 静止
+		 */
+		STATE_STATIC(0),
+		/**
+		 * 屏幕向下，即手指向上产生的滑动
+		 */
+		STATE_SCROLLED_UP(1),
+		/**
+		 * 屏幕向上，即手指向下产生的滑动
+		 */
+		STATE_SCROLLED_DONW(2);
+
+		private int nativeInt;
+
+		private SCROLL_STATE(int nativeInt) {
+			this.nativeInt = nativeInt;
+		}
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right,
+			int bottom) {
+		int mTotalHeight = 0;
+
+		// 遍历所有子视图
+		int childCount = getChildCount();
+		for (int i = 0; i < childCount; i++) {
+			View childView = getChildAt(i);
+
+			// 获取在onMeasure中计算的视图尺寸
+			int measureHeight = childView.getMeasuredHeight();
+			int measuredWidth = childView.getMeasuredWidth();
+
+			childView.layout(left, mTotalHeight, measuredWidth, mTotalHeight
+					+ measureHeight);
+
+			mTotalHeight += measureHeight;
+
+			Log.e("aaaaaaaaaaaaaa", "changed = " + changed + ", left = " + left
+					+ ", top = " + top + ", right = " + right + ", bottom = "
+					+ bottom + ", measureWidth = " + measuredWidth
+					+ ", measureHieght = " + measureHeight);
+		}
 	}
 }
